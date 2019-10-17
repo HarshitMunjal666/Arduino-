9716290964b992dda7ba1658a5020a8c1c21b818
deepnashu jain <deepanshuj.2610@gmail.com>
2019-10-17 22:36:36 +0530

    arduino

---
+++ sketch_apr12a/sketch_apr12a/sketch_apr12a.ino
@@ -0,0 +1,74 @@
+#include <RH_ASK.h>
+// Include dependant SPI Library 
+#include <SPI.h> 
+ 
+// Create Amplitude Shift Keying Object
+RH_ASK rf_driver;
+
+   
+void setup()
+{
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+    // Initialize ASK Object
+    rf_driver.init();
+    // Setup Serial Monitor
+    Serial.begin(9600);
+}
+ 
+void loop()
+{
+      // Set buffer to size of expected message
+    uint8_t buf[2];
+    uint8_t buflen = sizeof(buf);
+    // Check if received packet is correct size
+    if (rf_driver.recv(buf, &buflen))
+    {
+      
+      // Message received with valid checksum
+      Serial.print("Message Received: ");
+      Serial.println((char*)buf); 
+        if(buf[0]=='f')
+        {
+            digitalWrite(3,1);
+            digitalWrite(4,0);
+            digitalWrite(5,1);
+            digitalWrite(6,0);
+            delay(10);
+        }
+        else if(buf[0]=='b')
+        {
+         digitalWrite(3,0);
+         digitalWrite(4,1);
+         digitalWrite(5,0);
+         digitalWrite(6,1);
+         delay(10); 
+         }
+         else if(buf[0]=='r')
+         {
+         digitalWrite(3,1);
+         digitalWrite(4,0);
+         digitalWrite(5,0);
+         digitalWrite(6,0);
+         delay(10);
+         }
+         else  if(buf[0]=='l')
+         {
+         digitalWrite(3,0);
+         digitalWrite(4,0);
+         digitalWrite(5,1);
+         digitalWrite(6,0);
+         delay(10);
+         }
+         else
+         {
+         digitalWrite(3,0);
+         digitalWrite(4,0);
+         digitalWrite(5,0);
+         digitalWrite(6,0);
+         delay(10);
+         }
+    }
+}
